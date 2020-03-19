7c3658f473975ce30072d589846bda366d0f1052
Trần Đức Trung <38934471+niits@users.noreply.github.com>
2020-03-19 18:16:02 +0700

    complete gui

--- Week01/main.py
+++ Week01/main.py
@@ -1,42 +1,57 @@
 import sys
+import threading
 import urllib.request
+import wave
+from pprint import pprint
+
+import pyaudio
 import sounddevice as sd
 import os
 import re
 
-from PyQt5.QtWidgets import QMessageBox
+from PyQt5.QtWidgets import QMessageBox, QFileDialog
 from slugify import slugify
-from PyQt5 import QtWidgets, uic
+from PyQt5 import QtWidgets, uic, QtCore, QtGui
 from parsel import Selector
 
 
 class Ui(QtWidgets.QMainWindow):
-    def __init__(self):
+    def __init__(self, chunk=3024, frmat=pyaudio.paInt16, channels=2, rate=44100, py=pyaudio.PyAudio()):
         super(Ui, self).__init__()
         uic.loadUi('gui.ui', self)
-        self.recording = False
-        self.fs = 44100
+
         self.getContentButton = self.findChild(QtWidgets.QPushButton, 'getContentButton')
         self.recordButton = self.findChild(QtWidgets.QPushButton, 'recordButton')
-        self.goPrevButton = self.findChild(QtWidgets.QPushButton, 'goPrevButton')
+        self.stopButton = self.findChild(QtWidgets.QPushButton, 'stopButton')
         self.goNextButton = self.findChild(QtWidgets.QPushButton, 'goNextButton')
         self.saveAndQuitButton = self.findChild(QtWidgets.QPushButton, 'saveAndQuitButton')
 
         self.urlInput = self.findChild(QtWidgets.QLineEdit, 'urlInput')
+        self.textInput = self.findChild(QtWidgets.QPlainTextEdit, 'textInput')
 
         self.contentBrowser = self.findChild(QtWidgets.QTextBrowser, 'contentBrowser')
-        self.currentSentenceBrowser = self.findChild(QtWidgets.QTextBrowser, 'currentSentenceBrowser')
 
         self.getContentButton.clicked.connect(self.get_html_content)
-        self.recordButton.clicked.connect(self.on_record_button_click)
+        self.recordButton.clicked.connect(self.start_record)
+        self.stopButton.clicked.connect(self.end_record)
         self.goNextButton.clicked.connect(self.go_next)
-        self.goPrevButton.clicked.connect(self.go_prev)
         self.saveAndQuitButton.clicked.connect(self.save_and_quit)
 
+        self.CHUNK = chunk
+        self.FORMAT = frmat
+        self.CHANNELS = channels
+        self.RATE = rate
+        self.p = py
+        self.frames = []
+        self.st = 1
+        self.stream = self.p.open(format=self.FORMAT, channels=self.CHANNELS, rate=self.RATE, input=True,
+                                  frames_per_buffer=self.CHUNK)
+
         self.cleanR = re.compile('<.*?>|&([a-z0-9]+|#[0-9]{1,6}|#x[0-9a-f]{1,6});')
-        self.dir = ''
+        self.dir_path = ''
+        self.current_sentence = {}
         self.sentences = []
-        self.currentIndex = 0
+        self.thread = None
         self.show()
 
     def get_html_content(self):
@@ -60,11 +75,13 @@ class Ui(QtWidgets.QMainWindow):
                 fp.close()
                 sel = Selector(text=text)
                 title = sel.css('title').get()
-                self.dir = slugify(re.sub(self.cleanR, '', title))
-                if not os.path.exists('./records/' + self.dir):
-                    os.mkdir('./records/' + self.dir)
-                with open('./records/' + self.dir + '/index.txt', 'w') as f:
+                self.dir_path = slugify(re.sub(self.cleanR, '', title))
+
+                if not os.path.exists('./records/' + self.dir_path):
+                    os.mkdir('./records/' + self.dir_path)
+                with open('./records/' + self.dir_path + '/index.txt', 'w') as f:
                     f.write(url + '\n')
+
                 result = sel.css('article').get()
                 self.contentBrowser.append("""<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0//EN" 
                 "http://www.w3.org/TR/REC-html40/strict.dtd">\n<html><head><meta name="qrichtext" content="1" /><style 
@@ -72,42 +89,68 @@ class Ui(QtWidgets.QMainWindow):
                 Dlg 2'; font-size:8.25pt; font-weight:400; font-style:normal;">\n<p style="-qt-paragraph-type:empty; 
                 margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; 
                 text-indent:0px;">""" + result + """<br /></p></body></html>""")
-                cleantext = re.sub(self.cleanR, '', result)
-                cleantext = re.sub(r"[\n\t\xa0]*", '', cleantext)
-                sentences = cleantext.split('.')
-                self.sentences = [{'text': element, 'file': ''} for element in sentences]
-                self.currentSentenceBrowser.setText(self.sentences[self.currentIndex]['text'])
-
-    def go_next(self):
-        self.currentIndex = (self.currentIndex + 1) % len(self.sentences)
-        self.currentSentenceBrowser.setText(self.sentences[self.currentIndex]['text'])
-
-    def go_prev(self):
-        self.currentIndex = (self.currentIndex - 1 + len(self.sentences)) % len(self.sentences)
-        self.currentSentenceBrowser.setText(self.sentences[self.currentIndex]['text'])
-
-    def on_record_button_click(self):
-        if self.recording:
-            self.recordButton.setText('Ghi')
-            self.recording = False
-            self.end_record()
-        else:
-            self.recordButton.setText('Dừng Ghi')
-            self.recording = True
-            self.start_record()
 
     def save_and_quit(self):
-        f = open('./records/' + self.dir + '/index.txt', 'a', encoding='utf-8')
-        for block in self.sentences:
-            f.write(block['text'] + '\n')
-            f.write(block['file'] + '\n')
-        f.close()
-        self.close()
+        try:
+            print(self.sentences)
+            f = open('./records/' + self.dir_path + '/index.txt', 'a', encoding='utf-8')
+            for block in self.sentences:
+                f.write(block['text'] + '\n')
+                f.write(block['file'] + '\n')
+            f.close()
+            self.close()
+        except Exception as e:
+            print(e)
+
+    def go_next(self):
+        self.textInput.setPlainText('')
+
     def start_record(self):
-        print(self.currentIndex + 'start_record')
+        self.thread = threading.Thread(target=self.record)
+        self.thread.setDaemon(True)
+        try:
+            self.thread.start()
+        except (KeyboardInterrupt, SystemExit):
+            sys.exit()
+
+    def record(self):
+        text = self.textInput.toPlainText()
+        if text:
+            print('1' if text else 'clgt')
+            file_name = slugify(re.sub(self.cleanR, '', text)) + '.wav'
+            self.st = 1
+            self.frames = []
+            stream = self.p.open(format=self.FORMAT, channels=self.CHANNELS, rate=self.RATE, input=True,
+                                 frames_per_buffer=self.CHUNK)
+            while True:
+                data = stream.read(self.CHUNK)
+                self.frames.append(data)
+                print("* recording " + str(self.st))
+                if not self.st:
+                    break
+
+            stream.close()
+
+            wf = wave.open('./records/' + self.dir_path + '/' + file_name, 'wb')
+            wf.setnchannels(self.CHANNELS)
+            wf.setsampwidth(self.p.get_sample_size(self.FORMAT))
+            wf.setframerate(self.RATE)
+            wf.writeframes(b''.join(self.frames))
+            wf.close()
+
+            self.current_sentence['text'] = text
+            self.current_sentence['file'] = file_name
 
     def end_record(self):
-        print(self.currentIndex + 'end_record')
+        if threading.active_count() > 0:
+            self.st = 0
+            self.thread.join()
+            self.sentences.append(self.current_sentence)
+            self.current_sentence = {}
+            pprint(self.sentences)
+
+    def closeEvent(self, event):
+        print(threading.active_count())
 
 
 app = QtWidgets.QApplication(sys.argv)

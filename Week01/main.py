90ff0a330d71e9a37831aed53ff183d8254dbe41
Trần Đức Trung <38934471+niits@users.noreply.github.com>
2020-03-22 14:42:43 +0700

    fix get article content

--- Week01/main.py
+++ Week01/main.py
@@ -84,7 +84,7 @@ class Ui(QtWidgets.QMainWindow):
                 with open(self.dir_path + os.sep + 'index.txt', 'w') as f:
                     f.write(url + '\n')
 
-                result = sel.css('article').get()
+                result = sel.css('section.sidebar_1').get()
                 self.contentBrowser.setHtml("""<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0//EN" 
                 "http://www.w3.org/TR/REC-html40/strict.dtd">\n<html><head><meta name="qrichtext" content="1" /><style 
                 type="text/css">\np, li { white-space: pre-wrap; }\n</style></head><body style=" font-family:'MS Shell 
